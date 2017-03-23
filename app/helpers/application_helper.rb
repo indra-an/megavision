0425d774b6ca9c6e5b8dd554bf86fb15b873bcb2
Rifqi Fauzi <rifqi@Rifqis-MacBook-Pro.local>
2017-03-23 13:46:24 +0700

    dynamic price tag

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,12 @@ module ApplicationHelper
       (_action_1.nil? || controller.action_name.eql?(_action_1.to_s) ||
         controller.action_name.eql?(_action_2.to_s)) ? 'active' : ''
   end
+
+  def format_number_to_price_tag(number)
+    thousands = number.to_i / 1000
+    hundreds = number.to_i - (thousands * 1000)
+    hundreds = hundreds == 0 ? '000' : hundreds.to_s
+    raw '<sup class="font-18">Rp</sup>' + number_with_delimiter(thousands, :delimiter => '.') +
+        '.<sup class="font-18">' + hundreds + '</sup><sub class="font-12">/BLN</sub>'
+  end
 end
