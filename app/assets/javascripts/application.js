9068af2dd9ebf670bdf5ac4dc17018be3138c362
Rifqi Fauzi <rifqi@Rifqis-MacBook-Pro.local>
2017-03-24 09:02:27 +0700

    channel detail and smooth scroll

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,18 @@
 //= require jquery_ujs
 //= require shared/bootstrap.min
 //= require turbolinks
+
+$(document).on('turbolinks:load', function() {
+  // smooth scroll
+  $("a[href^='#']").on('click', function(e) {
+    e.preventDefault();
+
+    var hash = this.hash;
+
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 500, function() {
+      window.location.hash = hash;
+    });
+  });
+});
