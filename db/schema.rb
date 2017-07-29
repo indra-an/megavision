d17ae98d8b9149d62ad456757d79a2015d30d684
Indra <indra.abdullah99@gmail.com>
2017-07-29 11:28:17 +0700

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170721160944) do
+ActiveRecord::Schema.define(version: 20170729034420) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -166,6 +166,7 @@ ActiveRecord::Schema.define(version: 20170721160944) do
     t.text     "answer"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "attachment"
   end
 
   create_table "vacancies", force: :cascade do |t|
